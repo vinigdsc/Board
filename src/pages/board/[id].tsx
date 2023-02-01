50a952d29dff74236f9db3a3de8948e4207cea65
Vinicius Caetano <viniciusgcae@gmail.com>
2023-02-01 08:57:47 -0300

    editar task completo

--- src/pages/board/[id].tsx
+++ src/pages/board/[id].tsx
@@ -1,15 +1,87 @@
 import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/client";
+import firebase from "../../services/firebaseConnection";
+import { format } from "date-fns";
 
-export default function Task() {
+import Head from "next/head";
+import styles from "./task.module.scss";
+import { FiCalendar } from "react-icons/fi";
+
+type Task = {
+  id: string;
+  created: string | Date;
+  createdFormated?: string;
+  tarefa: string;
+  userId: string;
+  nome: string;
+};
+
+interface TaskListProps {
+  data: string;
+}
+
+export default function Task({ data }: TaskListProps) {
+  const task = JSON.parse(data) as Task;
+
   return (
-    <div>
-      <h1>paginas detalhes</h1>
-    </div>
+    <>
+      <Head>
+        <title>Detalhes da sua tarefa</title>
+      </Head>
+      <article className={styles.container}>
+        <div className={styles.actions}>
+          <div>
+            <FiCalendar size={30} color="#FFF" />
+            <span>Tarefa criada:</span>
+            <time>{task.createdFormated}</time>
+          </div>
+        </div>
+        <p>{task.tarefa}</p>
+      </article>
+    </>
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  params,
+}) => {
+  const { id } = params;
+  const session = await getSession({ req });
+
+  if (!session?.id) {
+    return {
+      redirect: {
+        destination: "/board",
+        permanent: false,
+      },
+    };
+  }
+
+  const data = await firebase
+    .firestore()
+    .collection("tarefas")
+    .doc(String(id))
+    .get()
+    .then((snapshot) => {
+      const data = {
+        id: snapshot.id,
+        created: snapshot.data().created,
+        createdFormated: format(
+          snapshot.data().created.toDate(),
+          "dd MMMM yyyy k:m"
+        ),
+        tarefa: snapshot.data().tarefa,
+        userId: snapshot.data().userId,
+        nome: snapshot.data().nome,
+      };
+
+      return JSON.stringify(data);
+    });
+
   return {
-    props: {},
+    props: {
+      data,
+    },
   };
 };
